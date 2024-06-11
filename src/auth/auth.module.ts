ecf494f92859a67ee74e44bc3ab10b0d52e8e70c
Alfin Prasetya <alfin.prasetya24@gmail.com>
2024-06-11 15:02:57 +0700

    Fix: add .env.example file

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -13,7 +13,7 @@ import { ConfigService } from '@nestjs/config';
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         secret: configService.get('JWT_SECRET'),
-        signOptions: { expiresIn: '1h' },
+        signOptions: { expiresIn: '15m' },
         global: true,
       }),
     }),
